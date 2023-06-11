35a969d7a1512f1057ad412cd7cfb906e9854fda
Belal Abulabn <bilal.net1996@gmail.com>
2023-06-11 13:29:14 +0200

    Create Signal Generator API file

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-from picoscope_module import PicoScopeSetup, SetChannel
+# from Open4000SeriesPicoScope import PicoScopeSetup, SetChannel
+from ps4000aSigGen import PicoScope4000a
+# import time
+# # Initialize the PicoScope
+# pico_scope = PicoScopeSetup()
+# pico_scope.open_unit()
 
-# Initialize the PicoScope
-pico_scope = PicoScopeSetup()
-pico_scope.open_unit()
+# # Initialize the SetChannel
+# set_channel = SetChannel(pico_scope)
 
-# Initialize the SetChannel
-set_channel = SetChannel(pico_scope)
+# # Call setup_channels function
+# set_channel.setup_channels()
 
-# Call setup_channels function
-set_channel.setup_channels()
+# # Close the PicoScope when done
+# pico_scope.close_unit()
+import time
+# from picoscope_4000a import PicoScope4000a
 
-# Close the PicoScope when done
-pico_scope.close_unit()
-#gg 
-#ghgg
+# Create an instance of the PicoScope4000a class
+picoscope = PicoScope4000a()
+
+# Open the device
+picoscope.open_device()
+
+# Use the set_signal_generator method to set various signal outputs
+picoscope.set_signal_generator('PS4000A_SINE', 10000, 10000)
+time.sleep(10)
+picoscope.set_signal_generator('PS4000A_SQUARE', 10000, 10000)
+time.sleep(10)
+picoscope.set_signal_generator('PS4000A_SQUARE', 10000, 100000, 5000, 1, 'UPDOWN')
+time.sleep(36)
+
+# Close the PicoScope
+status = picoscope.close()
+
+# Print the status
+print(status)
