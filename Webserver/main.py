e4b3acd6eb6666e6dd9e0ebe2854842d2a217d0b
abulabn <bilal.net1996@gmail.com>
2023-06-15 17:46:09 +0200

    adding Singnal Genartor Button

--- Webserver/main.py
+++ Webserver/main.py
@@ -1,7 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask, request, jsonify, render_template
+import ctypes
+from picosdk.ps4000a import ps4000a as ps
+from picosdk.functions import assert_pico_ok
+import sys
+sys.path.append('..')
+from GUI.ps4000aSigGen import PicoScope4000a
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+picoscope = PicoScope4000a()
+picoscope.open_device()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/set_signal_generator', methods=['POST'])
+def set_signal_generator():
+    data = request.json
+    wavetype = data['wavetype']
+    start_freq = data['start_freq']
+    stop_freq = data['stop_freq']
+    increment = data['increment']
+    dwell_time = data['dwell_time']
+    sweep_type = data['sweep_type']
+    pk_to_pk = data['pk_to_pk']
+
+    picoscope.set_signal_generator(wavetype, start_freq, stop_freq, increment, dwell_time, sweep_type, pk_to_pk)
+
+    return jsonify({'status': 'success'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Webserver/static/script.js
+++ Webserver/static/script.js
@@ -1,7 +1,38 @@
 function showTime() {
 	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+	
 }
 showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+function setSignalGenerator() {
+	var data = {
+		wavetype: document.getElementById("waveType").value,
+		start_freq: document.getElementById("startFreq").value,
+		stop_freq: document.getElementById("stopFreq").value,
+		increment: document.getElementById("increment").value,
+		dwell_time: document.getElementById("dwellTime").value,
+		sweep_type: document.getElementById("sweepType").value,
+		pk_to_pk: document.getElementById("peakToPeakVoltage").value
+	};
+
+	$.ajax({
+		url: '/set_signal_generator',
+		type: 'post',
+		contentType: 'application/json',
+		data: JSON.stringify(data),
+		success: function(response) {
+			console.log(response);
+		}
+	});
+}
+function onButtonClick() {
+	try {
+		// Call the function that might throw an error
+		setSignalGenerator();
+	} catch (error) {
+		// Display an alert box with the error message
+		alert(error.message);
+	}
+}
