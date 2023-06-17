60d1cffa1e182b2c4b99023af6093c91de227857
abulabn <bilal.net1996@gmail.com>
2023-06-17 14:24:40 +0200

    Fixing an bug in the signal generator_button

--- Webserver/static/script.js
+++ Webserver/static/script.js
@@ -1,35 +1,15 @@
 
-function setSignalGenerator() {
-	var data = {
-		wavetype: document.getElementById("waveType").value,
-		start_freq: document.getElementById("startFreq").value,
-		stop_freq: document.getElementById("stopFreq").value,
-		increment: document.getElementById("increment").value,
-		dwell_time: document.getElementById("dwellTime").value,
-		sweep_type: document.getElementById("sweepType").value,
-		pk_to_pk: document.getElementById("peakToPeakVoltage").value
-	};
 
-	$.ajax({
-		url: '/set_signal_generator',
-		type: 'post',
-		contentType: 'application/json',
-		data: JSON.stringify(data),
-		success: function(response) {
-			console.log(response);
-		}
-	});
-}
 
-function onButtonClick() {
-	try {
-		// Call the function that might throw an error
-		setSignalGenerator();
-	} catch (error) {
-		// Display an alert box with the error message
-		alert(error.message);
-	}
-}
+// function onButtonClick() {
+// 	try {
+// 		// Call the function that might throw an error
+// 		setSignalGenerator();
+// 	} catch (error) {
+// 		// Display an alert box with the error message
+// 		alert(error.message);
+// 	}
+// }
 
 function attemptConnection() {
 	fetch('/connect_device', {
@@ -45,12 +25,12 @@ function attemptConnection() {
 	})
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.querySelector("button");
-    if (button) {
-        button.addEventListener("click", attemptConnection);
-    }
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     var button = document.querySelector("button");
+//     if (button) {
+//         button.addEventListener("click", attemptConnection);
+//     }
+// });
 
 
 // document.addEventListener("DOMContentLoaded", function() {
@@ -99,46 +79,141 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var ctx = document.getElementById('signalChart').getContext('2d');
-	// Generate sine wave data
-	var labels = [];
-	var data = [];
-	for (var i = 0; i < 360; i += 10) { // 360 degrees, incrementing by 10 degrees
-		var radians = i * (Math.PI / 180); // Convert degrees to radians
-		var sineValue = Math.sin(radians);
-		labels.push(i);
-		data.push(sineValue);
-	}
+// document.addEventListener("DOMContentLoaded", function() {
+//     var ctx = document.getElementById('signalChart').getContext('2d');
+// 	// Generate sine wave data
+// 	var labels = [];
+// 	var data = [];
+// 	for (var i = 0; i < 360; i += 10) { // 360 degrees, incrementing by 10 degrees
+// 		var radians = i * (Math.PI / 180); // Convert degrees to radians
+// 		var sineValue = Math.sin(radians);
+// 		labels.push(i);
+// 		data.push(sineValue);
+// 	}
     
-    var timeScale = 's'; // You can change this to 'us' or 'ns' or have this set through UI
-    var voltageScale = 'V'; // You can change this to 'uV' or have this set through UI
+//     var timeScale = 's'; // You can change this to 'us' or 'ns' or have this set through UI
+//     var voltageScale = 'V'; // You can change this to 'uV' or have this set through UI
 
-    // Sample data
-    var timeData = []; // Assume this is in seconds
-    var voltageData = []; // Assume this is in volts
+//     // Sample data
+//     var timeData = []; // Assume this is in seconds
+//     var voltageData = []; // Assume this is in volts
 
-    // Convert time data based on scale
-    var convertedTimeData = timeData.map(function(time) {
-        switch (timeScale) {
-            case 'us':
-                return time * 1000000;
-            case 'ns':
-                return time * 1000000000;
-            default:
-                return time;
-        }
-    });
+//     // Convert time data based on scale
+//     var convertedTimeData = timeData.map(function(time) {
+//         switch (timeScale) {
+//             case 'us':
+//                 return time * 1000000;
+//             case 'ns':
+//                 return time * 1000000000;
+//             default:
+//                 return time;
+//         }
+//     });
 
-    // Convert voltage data based on scale
-    var convertedVoltageData = voltageData.map(function(voltage) {
-        switch (voltageScale) {
-            case 'uV':
-                return voltage * 1000000;
-            default:
-                return voltage;
-        }
-    });
+//     // Convert voltage data based on scale
+//     var convertedVoltageData = voltageData.map(function(voltage) {
+//         switch (voltageScale) {
+//             case 'uV':
+//                 return voltage * 1000000;
+//             default:
+//                 return voltage;
+//         }
+//     });
+
+//     var signalChart = new Chart(ctx, {
+//         type: 'line',
+//         data: {
+//             labels: labels,
+//             datasets: [{
+//                 label: 'Signal',
+//                 data: data,
+//                 borderColor: 'rgba(75, 192, 192, 1)',
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 x: {
+//                     title: {
+//                         display: true,
+//                         text: 'Time (' + timeScale + ')'
+//                     }
+//                 },
+//                 y: {
+//                     title: {
+//                         display: true,
+//                         text: 'Voltage (' + voltageScale + ')'
+//                     },
+//                     beginAtZero: false
+//                 }
+//             }
+//         }
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to set the signal generator
+    function setSignalGenerator() {
+        var waveTypeElement = document.getElementById("waveType");
+        var startFreqElement = document.getElementById("startFreq");
+        var stopFreqElement = document.getElementById("stopFreq");
+        var incrementElement = document.getElementById("increment");
+        var dwellTimeElement = document.getElementById("dwellTime");
+        var sweepTypeElement = document.getElementById("sweepType");
+        var peakToPeakVoltageElement = document.getElementById("peakToPeakVoltage");
+        
+        var data = {
+            wavetype: waveTypeElement ? waveTypeElement.value : null,
+            start_freq: startFreqElement ? startFreqElement.value : null,
+            stop_freq: stopFreqElement ? stopFreqElement.value : null,
+            increment: incrementElement ? incrementElement.value : null,
+            dwell_time: dwellTimeElement ? dwellTimeElement.value : null,
+            sweep_type: sweepTypeElement ? sweepTypeElement.value : null,
+            pk_to_pk: peakToPeakVoltageElement ? peakToPeakVoltageElement.value : null
+        };
+        
+        console.log("Data to be sent:", data);
+
+        $.ajax({
+            url: '/set_signal_generator',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function(response) {
+                console.log("Response from server:", response);
+            }
+        });
+    }
+    
+    // // Function to handle the button click
+    // function onButtonClick() {
+    //     try {
+    //         setSignalGenerator();
+    //     } catch (error) {
+    //         alert(error.message);
+    //     }
+    // }
+    
+    // Attach event listener to the button
+	// Attach event listener to the button
+	var signalButton = document.querySelector("#setSignalGeneratorButton");
+	if (signalButton) {
+		signalButton.addEventListener("click", setSignalGenerator);
+	}
+
+    // Initialize the chart
+    var ctx = document.getElementById('signalChart').getContext('2d');
+    var labels = [];
+    var data = [];
+    for (var i = 0; i < 360; i += 10) {
+        var radians = i * (Math.PI / 180);
+        var sineValue = Math.sin(radians);
+        labels.push(i);
+        data.push(sineValue);
+    }
+
+    var timeScale = 's';
+    var voltageScale = 'V';
 
     var signalChart = new Chart(ctx, {
         type: 'line',
@@ -170,4 +245,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
-
